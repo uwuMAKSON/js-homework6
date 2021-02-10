98fb0eb7b7e82d86695015cb0d555948e402e6b7
Maksim Pyzhin <maksimkarybaev@gmail.com>
2021-02-10 17:06:43 +0600

    added minus

--- scripts.js
+++ scripts.js
@@ -13,20 +13,20 @@ function plus() {
     document.getElementById('out').innerHTML = result;
 }
 
-// function minus() {
-//     var num1, num2, result;
+function minus() {
+    var num1, num2, result;
 
-//     num1 = document.getElementById('n1').value;
-//     num1 = parseInt(num1);
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
 
 
-//     num2 = document.getElementById('n2').value;
-//     num2 = parseInt(num2);
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2);
 
-//     result = num1 - num2;
+    result = num1 - num2;
 
-//     document.getElementById('out').innerHTML = result;
-// }
+    document.getElementById('out').innerHTML = result;
+}
 
 // function multiply() {
 //     var num1, num2, result;
