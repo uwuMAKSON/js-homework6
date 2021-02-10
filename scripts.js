08255c20c600807a0f745b84648a86655d29dbfe
Maksim Pyzhin <maksimkarybaev@gmail.com>
2021-02-10 17:07:07 +0600

    added multiplay

--- scripts.js
+++ scripts.js
@@ -28,20 +28,20 @@ function minus() {
     document.getElementById('out').innerHTML = result;
 }
 
-// function multiply() {
-//     var num1, num2, result;
+function multiply() {
+    var num1, num2, result;
 
-//     num1 = document.getElementById('n1').value;
-//     num1 = parseInt(num1);
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
 
 
-//     num2 = document.getElementById('n2').value;
-//     num2 = parseInt(num2);
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2);
 
-//     result = num1 * num2;
+    result = num1 * num2;
 
-//     document.getElementById('out').innerHTML = result;
-// }
+    document.getElementById('out').innerHTML = result;
+}
 
 // function razdel() {
 //     var num1, num2, result;
